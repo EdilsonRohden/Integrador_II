0af7b97eece8a89b58fb8f53aeaec537d60b4a57
EdilsonRohden <edilsonrohden11@gmail.com>
2019-06-11 21:47:12 -0300

    Alterações no script do DB para formulario do cliente.

--- Script estrutura BD.sql
+++ Script estrutura BD.sql
@@ -29,7 +29,7 @@ CREATE TABLE cliente (
 	bairro VARCHAR(70),
 	cep VARCHAR(10),
 	fone VARCHAR(15),
-	email VARCHAR(70),
+	email VARCHAR(70)
 	excluido BOOLEAN DEFAULT FALSE
 );
 
@@ -66,7 +66,6 @@ CREATE TABLE movimento_alteracao (
 INSERT INTO primeiro_nivel (id, descricao) VALUES
 (1, 'Crédito'),
 (2, 'Débito');
-
 
 
 
